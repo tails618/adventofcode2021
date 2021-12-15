df26a17ff8ebef08e63f8b732df69bd30ee8d505
tails618 <asher.grossman@gmail.com>
2021-12-15 11:10:42 -0600

    Removed commented code

--- 1/1-1.py
+++ 1/1-1.py
@@ -4,9 +4,6 @@ with open('input.txt', 'r') as f:
 #for each item, check if it is greater than the previous item
 #if it is, increment numIncrements
 numIncrements = 0
-'''for i in range(1,len(file)):
-    if file[i] > file[i - 1]:
-        numIncrements += 1'''
 for i, j in enumerate(file):
     if j > file[i-1]:
         numIncrements += 1
